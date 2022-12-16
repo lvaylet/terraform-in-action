9f5425c602625c0cf8d039902274348ca60b4b6f
Laurent VAYLET <laurent.vaylet@gmail.com>
2022-12-16 16:20:13 +0100

    create BigQuery dataset

--- main.tf
+++ main.tf
@@ -26,3 +26,15 @@ resource "google_project_service" "project" {
   service                    = each.key
   disable_dependent_services = true
 }
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = "example_dataset"
+  friendly_name               = "test"
+  description                 = "This is a test description"
+  location                    = "EU"
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+}
